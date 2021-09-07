36e456ad2751fce028d79ddb51d9183908f5cd43
Sumeet Agarwal <sumeet.a@gmail.com>
2021-09-07 09:58:32 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,27 @@ use rdev::{listen, Event, EventType};
 //use timer::Timer;
 use std::process::Command;
 
-const TOP_BAR_HEIGHT : f64 = 55.0;
+const TOP_BAR_SHOW_THRESHOLD : f64 = 0.0;
+const TOP_BAR_HIDE_THRESHOLD : f64 = 70.0;
+
 fn main() {
     let mut show_topbar_cmd: Command =
         Command::new("/home/sumeet/bin/bspwm-show-topbar");
     let mut hide_topbar_cmd =
         Command::new("/home/sumeet/bin/bspwm-hide-topbar");
 
-    //let timer_maker = Timer::new();
-    let mut was_already_top = None;
+    let mut is_top_bar_shown = None;
     let callback = move |event: Event| {
         match event.event_type {
             EventType::MouseMove { x: _, y } => {
-                let mouse_in_top_now = y <= TOP_BAR_HEIGHT;
-                match (mouse_in_top_now, was_already_top) {
-                    (true, None | Some(false)) => {
+                match is_top_bar_shown {
+                    None | Some(false) if y <= TOP_BAR_SHOW_THRESHOLD => {
                         show_topbar_cmd.output().unwrap();
-                        was_already_top = Some(true);
+                        is_top_bar_shown = Some(true);
                     }
-                    (false, None | Some(true)) => {
+                    None | Some(true) if y > TOP_BAR_HIDE_THRESHOLD => {
                         hide_topbar_cmd.output().unwrap();
-                        was_already_top = Some(false);
+                        is_top_bar_shown = Some(false);
                     }
                     _ => (),
                 }
